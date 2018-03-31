469aa4d29293b7c547cdf5751f6982d321f25c4c
Nick Hall <nick.hall@bluelogicteam.com>
2018-03-31 13:33:24 -0500

    final solution

--- breadCrumbGenerator.js
+++ breadCrumbGenerator.js
@@ -1,123 +1,78 @@
-class BreadCrumbGenerator {
-	constructor(url, separator) {
-		this.url = url;
-		console.log('this.url:', this.url);
-		this.file;
-		this.separator = ` ${separator.trim().charAt(0)} `;
-		this.ignoreArr = ['the','of','in','from','by','with','and', 'or', 'for', 'to', 'at', 'a'];
+const ignoreArr = ['the', 'of', 'in', 'from', 'by', 'with', 'and', 'or', 'for', 'to', 'at', 'a'];
 
-		this.linksArr;
-		this.filesArr;
+function produceSpan(content) {
+	return `<span class="active">${formatTitle(content)}</span>`
+}
+
+function formatTitle(content) {
+	if (content.length > 30) {
+		return content.split('-').map(c => ignoreArr.indexOf(c) === -1 ? c.charAt(0).toUpperCase() : false).filter(v => !!v).join('');
 	}
+	return content.replace(/\-/g, ' ').toUpperCase();
+}
 
-	removeAnyFromUrl(arr) {
-		for (let i = 0; i < arr.length; i++) {
-			const item = arr[i];
-			console.log({ item })
-			const itemRegEx = new RegExp(`${item}`, 'g');
-
-			if (this.url.match(itemRegEx)) {
-				this.url = this.url.replace(itemRegEx, ' ');
-			}
-		}
-	}
-
-	removeLinksFromUrl() {
-		this.linksArr = this.url.split('/').filter((v, index, arr) => {
-			console.log({ v });
-			if (v !== '' && !/(http|https)\:?/.test(v) && !/(index)/.test(v) && index !== arr.length - 1) return v;
-			return;
-		});
-		this.home = this.linksArr[0];
-		this.linksArr.shift();
-		const homeRegEx = new RegExp(`${this.home}`, 'g');
-
-		this.url = this.url.replace(homeRegEx, '').trim();
-		this.removeAnyFromUrl(this.linksArr);
-		this.url = this.url.replace(/\/{0,}/g, '');
-
-		console.log('this.linksArr after removeLinksFromUrl: ', this.linksArr);
-	}
-
-	removeFilesFromUrl() {
-		console.log('this.url:', this.url);
-		this.filesArr = this.url.split(/\s{1,}/g);
-		console.log('this.filesArr:', this.filesArr);
-
-		if (!this.url.match(/index/g)) {
-			this.file = this.url
-		} else {
-			this.file = this.linksArr[this.linksArr.length - 1];
-			this.linksArr.pop();
-		}
-
-	}
-
-	generateSpan() {
-		return `<span class="active">${this.file.toUpperCase()}</span>`;
-	}
-
-	generateLinks() {
-		let str = '';
-		let acc = '';
-
-		for (let i = 0; i < this.linksArr.length; i++) {
-			let link = this.linksArr[i];
-			const isLongerThanThirty = link.length > 30;
-			const abbLink = link.split('-').map(l => {
-				if (this.ignoreArr.indexOf(l) > -1 && this.ignoreArr.find(f => f === l)) {
-					return null;
-				}
-				return l.charAt(0).toUpperCase();
-			}).join('');
-			if (i === 0) {
-				console.log({ i });
-				str += `<a href="/${link}/">${isLongerThanThirty ? abbLink : link.toUpperCase()}</a>${i + 1 === this.linksArr.length ? '' : this.separator}`;
-			} else {
-				console.log({ i });
-				if (i === this.linksArr.length) {
-					str += ` <a href="/${acc}">${isLongerThanThirty ? abbLink : link.toUpperCase()}</a> ${this.separator} `;
-				} else {
-					acc += `${link}/`;
-					str += `<a href="/${acc}">${isLongerThanThirty ? abbLink : link.toUpperCase()}</a>`
-				}
-			}
-			acc += `${link}/`;
-		}
-
-		return str;
-	}
-
-	generateResult() {
-		this.removeLinksFromUrl()
-		this.removeFilesFromUrl();
-
-		return `<a href="/">HOME</a>${this.separator}${this.generateLinks()}${this.separator}${this.generateSpan()}`;
-	}
+function produceAnchor(filePath, content) {
+	return `<a href="${filePath}">${formatTitle(content)}</a>`;
 }
 
 function generateBC(url, separator) {
-	const BCG = new BreadCrumbGenerator(url, separator);
+	let returnString = '';
+	separator = ` ${`${separator}`.trim()} `;
+	const splitArr = url.replace(/\D{0,6}:/, '').split('/').filter(u => u && u !== '' && u.indexOf('index.') < 0).map(url => {
+		if (url.match(/[?#]/)) {
+			return url.split(/[#?]/g)[0];
+		}
+		return url;
+	});
 
-	return BCG.generateResult();
+	return splitArr.reduce((acc, cur, index, arr) => {
+		const isLast = index === arr.length - 1;
+
+		if (index == 0) {
+			if (isLast) {
+				acc += produceSpan('HOME');
+				return acc;
+			}
+
+			acc += produceAnchor('/', 'HOME') + `${separator}`;
+			return acc;
+		}
+
+		if (isLast) {
+			acc += produceSpan(cur.replace(/\..+/g, ''));
+			return acc;
+		}
+
+		const url = arr.slice(1, index + 1).join('/'); // Define path to your link
+
+ 		acc += produceAnchor(`/${url}/`, cur) + `${separator}`;
+
+		return acc;
+	}, '');
 }
 
 
-// TESTS
-console.log('YOURS:', generateBC("www.microsoft.com/important/confidential/docs/index.htm#top", " * "), '\n\nSHOULD BE:', '<a href="/">HOME</a> * <a href="/important/">IMPORTANT</a> * <a href="/important/confidential/">CONFIDENTIAL</a> * <span class="active">DOCS</span>', '\n-------------------------');
-console.log('YOURS:', generateBC("mysite.com/very-long-url-to-make-a-silly-yet-meaningful-example/example.asp", " > "), '\n\nSHOULD BE:', '<a href="/">HOME</a> > <a href="/very-long-url-to-make-a-silly-yet-meaningful-example/">VLUMSYME</a> > <span class="active">EXAMPLE</span>', '\n-------------------------');
-console.log('YOURS:', generateBC("www.very-long-site_name-to-make-a-silly-yet-meaningful-example.com/users/giacomo-sorbi", " + "), '\n\nSHOULD BE:', '<a href="/">HOME</a> + <a href="/users/">USERS</a> + <span class="active">GIACOMO SORBI</span>', '\n-------------------------');
+const Test = { assertValue: (actual, expected) => { if (expected !== actual) throw new Error(`Expected: ${expected}\nGot: ${actual}`); console.info('SUCCESS!') } };
 
-console.log('YOURS:', generateBC('www.codewars.com/users/GiacomoSorbi?ref=CodeWars', ' / '), '\nSHOULD BE: ', '<a href="/">HOME</a> / <a href="/users/">USERS</a> / <span class="active">GIACOMOSORBI</span>', '\n-------------------------');
-console.log('YOURS:', generateBC('https://www.linkedin.com/in/giacomosorbi', ' * '), '\nSHOULD BE: ', '<a href="/">HOME</a> * <a href="/in/">IN</a> * <span class="active">GIACOMOSORBI</span>', '\n-------------------------');
-console.log('YOURS:', generateBC('www.agcpartners.co.uk/', ' * '), '\nSHOULD BE: ', '<span class="active">HOME</span>', '\n-------------------------');
-console.log('YOURS:', generateBC('www.agcpartners.co.uk', ' # '), '\nSHOULD BE: ', '<span class="active">HOME</span>', '\n-------------------------');
-console.log('YOURS:', generateBC('https://www.agcpartners.co.uk/index.html', ' >>> '), '\nSHOULD BE:', '<span class="active">HOME</span>', '\n-------------------------');
-console.log('YOURS:', generateBC('facebook.fr/insider-a-or-with-the-immunity-skin-biotechnology/images/biotechnology-at-diplomatic-insider-bed-paper/web?order=desc&filter=adult', ' # '), '\nSHOULD BE: ', '<a href="/">HOME</a> # <a href="/insider-a-or-with-the-immunity-skin-biotechnology/">IISB</a> # <a href="/insider-a-or-with-the-immunity-skin-biotechnology/images/">IMAGES</a> # <a href="/insider-a-or-with-the-immunity-skin-biotechnology/images/biotechnology-at-diplomatic-insider-bed-paper/">BDIBP</a> # <span class="active">WEB</span>', '\n-------------------------');
+Test.assertValue(generateBC("www.microsoft.com/important/confidential/docs/index.htm#top", " * "), '<a href="/">HOME</a> * <a href="/important/">IMPORTANT</a> * <a href="/important/confidential/">CONFIDENTIAL</a> * <span class="active">DOCS</span>');
+Test.assertValue(generateBC("mysite.com/very-long-url-to-make-a-silly-yet-meaningful-example/example.asp", " > "), '<a href="/">HOME</a> > <a href="/very-long-url-to-make-a-silly-yet-meaningful-example/">VLUMSYME</a> > <span class="active">EXAMPLE</span>');
+Test.assertValue(generateBC("www.very-long-site_name-to-make-a-silly-yet-meaningful-example.com/users/giacomo-sorbi", " + "), '<a href="/">HOME</a> + <a href="/users/">USERS</a> + <span class="active">GIACOMO SORBI</span>');
 
-console.log('YOURS:', generateBC('http://www.codewars.com/images/in-skin-bed-immunity-for-paper-biotechnology/giacomo-sorbi.htm', ' ; '), '\nSHOULD BE: ', '<a href="/">HOME</a> ; <a href="/images/">IMAGES</a> ; <a href="/images/in-skin-bed-immunity-for-paper-biotechnology/">SBIPB</a> ; <span class="active">GIACOMO SORBI</span>', '\n-------------------------');
-console.log('YOURS:', generateBC('https://www.twitter.de/to-uber-immunity-surfer-a-research-biotechnology-meningitis/profiles', ' >>> '), '\nSHOULD BE: ', '<a href="/">HOME</a> > <a href="/to-uber-immunity-surfer-a-research-biotechnology-meningitis/">UISRBM</a> > <span class="active">PROFILES</span>', '\n-------------------------');
-console.log('YOURS:', generateBC('google.ca/a-paper-bioengineering-transmutation-pippi-kamehameha/games/games/users/test.php', ' >>> '), '\nSHOULD BE: ', '<a href="/">HOME</a> > <a href="/a-paper-bioengineering-transmutation-pippi-kamehameha/">PBTPK</a> > <a href="/a-paper-bioengineering-transmutation-pippi-kamehameha/games/">GAMES</a> > <a href="/a-paper-bioengineering-transmutation-pippi-kamehameha/games/games/">GAMES</a> > <a href="/a-paper-bioengineering-transmutation-pippi-kamehameha/games/games/users/">USERS</a> > <span class="active">TEST</span>', '\n-------------------------');
+Test.assertValue(generateBC('www.codewars.com/users/GiacomoSorbi?ref=CodeWars', ' / '), '<a href="/">HOME</a> / <a href="/users/">USERS</a> / <span class="active">GIACOMOSORBI</span>');
+Test.assertValue(generateBC('https://www.linkedin.com/in/giacomosorbi', ' * '), '<a href="/">HOME</a> * <a href="/in/">IN</a> * <span class="active">GIACOMOSORBI</span>');
+Test.assertValue(generateBC('www.agcpartners.co.uk/', ' * '), '<span class="active">HOME</span>');
+Test.assertValue(generateBC('www.agcpartners.co.uk', ' # '), '<span class="active">HOME</span>');
+Test.assertValue(generateBC('https://www.agcpartners.co.uk/index.html', ' >>> '), '<span class="active">HOME</span>');
+Test.assertValue(generateBC('facebook.fr/insider-a-or-with-the-immunity-skin-biotechnology/images/biotechnology-at-diplomatic-insider-bed-paper/web?order=desc&filter=adult', ' # '), '<a href="/">HOME</a> # <a href="/insider-a-or-with-the-immunity-skin-biotechnology/">IISB</a> # <a href="/insider-a-or-with-the-immunity-skin-biotechnology/images/">IMAGES</a> # <a href="/insider-a-or-with-the-immunity-skin-biotechnology/images/biotechnology-at-diplomatic-insider-bed-paper/">BDIBP</a> # <span class="active">WEB</span>');
 
-console.log('YOURS:', generateBC('linkedin.it/immunity-a-uber-transmutation-bioengineering#team', ' ; '), '\nSHOULD BE: ', '<a href="/">HOME</a> ; <span class="active">IUTB</span>', '\n-------------------------');
-console.log('YOURS:', generateBC('http://agcpartners.co.uk/most-downloaded/pictures-you-wished-you-never-saw-but-you-cannot-unsee-now/files/app', ' . '), '\nSHOULD BE: ', '<a href="/">HOME</a> . <a href="/most-downloaded/">MOST DOWNLOADED</a> . <a href="/most-downloaded/pictures-you-wished-you-never-saw-but-you-cannot-unsee-now/">PYWYNSBYCUN</a> . <a href="/most-downloaded/pictures-you-wished-you-never-saw-but-you-cannot-unsee-now/files/">FILES</a> . <span class="active">APP</span>', '\n-------------------------');
+Test.assertValue(generateBC('http://www.codewars.com/images/in-skin-bed-immunity-for-paper-biotechnology/giacomo-sorbi.htm', ' ; '), '<a href="/">HOME</a> ; <a href="/images/">IMAGES</a> ; <a href="/images/in-skin-bed-immunity-for-paper-biotechnology/">SBIPB</a> ; <span class="active">GIACOMO SORBI</span>');
+Test.assertValue(generateBC('https://www.twitter.de/to-uber-immunity-surfer-a-research-biotechnology-meningitis/profiles', ' >>> '), '<a href="/">HOME</a> > <a href="/to-uber-immunity-surfer-a-research-biotechnology-meningitis/">UISRBM</a> > <span class="active">PROFILES</span>');
+Test.assertValue(generateBC('google.ca/a-paper-bioengineering-transmutation-pippi-kamehameha/games/games/users/test.php', ' >>> '), '<a href="/">HOME</a> > <a href="/a-paper-bioengineering-transmutation-pippi-kamehameha/">PBTPK</a> > <a href="/a-paper-bioengineering-transmutation-pippi-kamehameha/games/">GAMES</a> > <a href="/a-paper-bioengineering-transmutation-pippi-kamehameha/games/games/">GAMES</a> > <a href="/a-paper-bioengineering-transmutation-pippi-kamehameha/games/games/users/">USERS</a> > <span class="active">TEST</span>');
+
+Test.assertValue(generateBC('linkedin.it/immunity-a-uber-transmutation-bioengineering#team', ' ; '), '<a href="/">HOME</a> ; <span class="active">IUTB</span>');
+Test.assertValue(generateBC('http://agcpartners.co.uk/most-downloaded/pictures-you-wished-you-never-saw-but-you-cannot-unsee-now/files/app', ' . '), '<a href="/">HOME</a> . <a href="/most-downloaded/">MOST DOWNLOADED</a> . <a href="/most-downloaded/pictures-you-wished-you-never-saw-but-you-cannot-unsee-now/">PYWYNSBYCUN</a> . <a href="/most-downloaded/pictures-you-wished-you-never-saw-but-you-cannot-unsee-now/files/">FILES</a> . <span class="active">APP</span>');
+
+
+Test.assertValue(generateBC('google.ca/or-with-pippi-a-biotechnology/web/transmutation-at-surfer-in-surfer-uber-bladder/app#info?rank=recent_first&hide=sold', ' ; '), '<a href="/">HOME</a> ; <a href="/or-with-pippi-a-biotechnology/">OR WITH PIPPI A BIOTECHNOLOGY</a> ; <a href="/or-with-pippi-a-biotechnology/web/">WEB</a> ; <a href="/or-with-pippi-a-biotechnology/web/transmutation-at-surfer-in-surfer-uber-bladder/">TSSUB</a> ; <span class="active">APP</span>');
+
+Test.assertValue(generateBC('pippi.pi/images/app?hack=off', ' # '), '<a href="/">HOME</a> # <a href="/images/">IMAGES</a> # <span class="active">APP</span>');
