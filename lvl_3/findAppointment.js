c143b42c2456526310ba358e857d384ab26c6f46
nickhallph <nick.hall@yum.com>
2018-04-02 11:56:39 -0500

    added instructions and rules

--- lvl_3/findAppointment.js
+++ lvl_3/findAppointment.js
@@ -13,7 +13,6 @@ function getStartTime(schedules, duration) {
 
       console.log({ employeeEarliest, employeeLatest });
     }
-
   }
 }
 
@@ -27,4 +26,21 @@ var schedulesArr = [
   [['11:30', '12:15'], ['15:00', '16:30'], ['17:45', '19:00']]
 ];
 console.log(getStartTime(schedulesArr, 60), '\n-------------\nPASSED:', getStartTime(schedulesArr, 60) === '12:15', '\n');
-console.log(getStartTime(schedulesArr, 90), '\n-------------\nPASSED:', getStartTime(schedulesArr, 90) === null, '\n');
+// console.log(getStartTime(schedulesArr, 90), '\n-------------\nPASSED:', getStartTime(schedulesArr, 90) === null, '\n');
+
+
+
+// INSTRUCTIONS
+//
+// The businesspeople among you will know that it's often not easy to find an appointment.
+// In this kata we want to find such an appointment automatically.
+// You will be given the calendars of our businessperson and a duration for the meeting.
+// Your task is to find the earliest time, when every businessperson is free for at least that duration.
+
+// RULES
+//
+// - All times in the calendars will be given in 24h format "hh:mm", the result must also be in that format
+// - A meeting is represented by its start time (inclusively) and end time (exclusively) -> if a meeting takes place from 09:00 - 11:00, the next possible start time would be 11:00
+// - The businesspeople work from 09:00 (inclusively) - 19:00 (exclusively), the appointment must start and end within that range
+// - If the meeting does not fit into the schedules, return null or None as result
+// - The duration of the meeting will be provided as an integer in minutes
