2a6f334b5e50c94b16b399938137d00ba2493bb3
Nick Hall <Nick.Hall@yum.com>
2018-04-16 15:43:38 -0500

    added info comment

--- lvl_4/IPValidator.js
+++ lvl_4/IPValidator.js
@@ -1,3 +1,7 @@
+// This was a level 4 Kata when I attempted it, and I remember saying "LOL, this is NOT a 4, this a level 6"... 
+// Well, now apparently this Kata is now a level 6 XD
+// Oh well, it was a level 4 when I attempted it, so it's staying in the lvl_4 directory :)
+
 function isValidIP(ip) {
   const splitIP = ip.split('.');
 
