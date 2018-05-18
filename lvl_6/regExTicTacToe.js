a3d37f27cde23faf9acc465690a8cde7e265bc16
nickhallph <nick.hall@yum.com>
2018-05-18 12:45:38 -0500

    initial commit

---
+++ lvl_6/regExTicTacToe.js
@@ -0,0 +1,45 @@
+const regexTicTacToeWinChecker = board => {
+  board = board.toLowerCase();
+
+  
+}
+
+
+
+
+
+
+
+
+
+// ---
+// ---
+// ---
+
+// 3 Different Wins
+
+// 1 - between [X|O]
+
+// --X
+// -X-
+// X--
+
+// 2 - between [X|O]
+
+// X--
+// X--
+// X--
+
+// -X-
+// -X-
+// -X-
+
+// --X
+// --X
+// --X
+
+// 3 - between [X|O]
+
+// X--
+// -X-
+// --X
