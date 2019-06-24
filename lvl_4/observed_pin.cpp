759477f7b1e2c5fc2eeac1e32872c331856db526
nickkhall <nick.hall@bluelogicteam.com>
2019-06-23 21:08:19 -0500

    final solution

--- lvl_4/observed_pin.cpp
+++ lvl_4/observed_pin.cpp
@@ -1,65 +1,37 @@
 #include <iostream>
-#include <string>
 #include <vector>
+#include <algorithm>
 
-void combinationUtil(int combo[], int temp[], int start, int end, int index, int pinLength) {  
-    // Current combination is ready 
-    // to be printed, print it  
-    if (index == pinLength)  { 
-        for (int j = 0; j < pinLength; j++) { 
-          std::cout << temp[j] << " ";  
-        }
-        
-        // PUSH TEMP ONTO FINAL ARRAY
-        std::cout << std::endl;
-        std::cout << "----------------------" << std::endl;
-        return;  
-    }  
-  
-    for (int i = start; i <= end && end - i + 1 >= pinLength - index; i++) {
-        std::cout << "index: " << index << std::endl;
-        std::cout << "temp[index]: " << temp[index] << std::endl;
-        std::cout << "combo[i] : " << combo[i] << std::endl;
-        temp[index] = combo[i];  
-        combinationUtil(combo, temp, i + 1, end, index + 1, pinLength);  
-    }  
-}  
+using namespace std;
 
-std::vector<std::string> get_pins(const std::string &observed) {
-  std::vector<std::string> registerVec {};
-  std::vector<std::vector<std::string>> accumVec {};
-  std::vector<std::string> finalVec {};
-  std::vector<std::vector<int>> possibilities {
-    {}, {3, 0, 1}, {3, -1, 0, 1}, {3, -1, 0},
-    {-3, 3, 0, 1}, {-3, 3, -1, 0, 1},
-    {-3, 3, -1, 0}, {-3, 0, 1}, {-8, -3, 0, -1, +1},
-    {-3, -1, 0}
-  };
-  
-  for (int o = 0; o < observed.length(); o++) {
-    const int cur = observed[o] - '0';
+vector<string> get_pins(string pin) {
+    vector<string> pins {};
+    const vector<vector<string>> pin_possibilities = {
+        // 0, 1, 2, 3
+        {"0", "8"}, {"1", "2", "4"}, {"1", "2", "3", "5"}, {"2", "3", "6"},
+        // 4, 5, 6
+        {"1", "4", "5", "7"}, {"2", "4", "5", "6", "8"}, {"3", "5", "6", "9"},
+        // 7, 8, 9
+        {"4", "7", "8"}, {"0", "5", "7", "8", "9"}, {"6", "8", "9"}
+    };
     
-    for (int p = 1; p < possibilities.size(); p++) {
-      if (cur == p) {
-        for (int poss : possibilities[p]) {
-          registerVec.emplace_back(std::to_string((cur + poss)));
-          registerVec.shrink_to_fit();
-        }
-      }
+    
+    if (pin.length() == 1) {
+        return pin_possibilities[atoi(pin.c_str())];
     }
     
-    accumVec.emplace_back(registerVec);
-    accumVec.shrink_to_fit();
-    registerVec.clear();
-    registerVec.shrink_to_fit();
-  }
-  
-  for (int a = 0; a < accumVec.size(); a++)
-    for (int x = 0; x < accumVec[a].size(); x++) std::cout << accumVec[a][x] << std::endl;
-  
-  return finalVec;
-}
-
-int main() {
-  
+    vector<string> sub_pins = get_pins(pin.substr(1));
+    
+    vector<string> cur_possibilities = pin_possibilities[(int)pin[0] - 48];
+    
+    for (int i = 0; i < cur_possibilities.size(); i++) {
+        for (int x = 0; x < sub_pins.size(); x++) {
+            string temp_cur = "";
+            temp_cur += cur_possibilities[i];
+            temp_cur += sub_pins[x];
+            pins.emplace_back(temp_cur);
+        }
+    }
+    
+    return pins;
 }
